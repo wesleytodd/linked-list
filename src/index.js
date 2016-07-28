bdb423b1b4f9c69e82f34c8af0ebcf870645f1eb
Wes Todd <wes@wesleytodd.com>
2016-07-27 21:51:46 -0500

    make it functional

--- src/index.js
+++ src/index.js
@@ -1,124 +1,129 @@
-const _head = Symbol('head');
-const _tail = Symbol('tail');
+export function List (items) {
+	if (!items) {
+		return null;
+	}
+	if (arguments.length > 1) {
+		return listFromArray(arguments);
+	}
+	if (Array.isArray(items)) {
+		return listFromArray(items);
+	}
+	return Node(items);
+}
 
-var LinkedList = module.exports = function () {
-	this[_head] = null;
-	this[_tail] = null;
-	this.length = 0;
-};
+function listFromArray (items) {
+	var head = null;
+	var i = items.length;
+	while (i--) {
+		var n = Node(items[i]);
+		n.next = head;
+		head = n;
+	}
+	return head;
+}
 
-LinkedList.node = function (data) {
+export function length (list) {
+	var length = 0;
+	var head = list;
+	while (head) {
+		length++;
+		head = head.next;
+	}
+	return length;
+}
+
+export function Node (data) {
 	return {
 		data: data,
 		next: null
 	};
-};
+}
 
-LinkedList.head = function (list) {
-	return list[_head] && list[_head].data || null;
-};
-
-LinkedList.tail = function (list) {
-	return list[_tail] && list[_tail].data || null;
-};
-
-LinkedList.prototype.append = function (data) {
-	var node = LinkedList.node(data);
-	if (!this[_head]) {
-		this[_head] = node;
+export function tail (list) {
+	while (list && list.next) {
+		list = list.next;
 	}
-	if (!this[_tail]) {
-		this[_tail] = node;
-	}
-	this[_tail].next = node;
-	this[_tail] = node;
-	this.length++;
-};
+	return list;
+}
 
-LinkedList.prototype.prepend = function (data) {
-	var node = LinkedList.node(data);
-	node.next = this[_head];
-	this[_head] = node;
-	if (!this[_tail]) {
-		this[_tail] = node;
-	}
-	this.length++;
-};
+export function append (list, item) {
+	var _tail = tail(list);
+	_tail && (_tail.next = Node(item));
+	return list;
+}
 
-LinkedList.prototype.remove = function (val) {
+export function prepend (list, item) {
+	var head = Node(item);
+	head.next = list;
+	return head;
+}
+
+export function forEach (list, fnc) {
+	var i = 0;
+	var head = list;
+	while (head) {
+		// Early return by returning false
+		// from forEach callback
+		if (fnc(head, i, list) === false) {
+			return;
+		}
+		i++;
+		head = head.next;
+	}
+	return list;
+}
+
+export function remove (list, val) {
 	// Since this is a singly linked list,
 	// we need to keep a reference to the last
 	// seen node to link after removing
 	var last = null;
+	var head = list;
 
-	this.forEach(function (item) {
-		// Found our item
-		if (item.data === val) {
-			// Item is head, just unlink head
-			if (item === this[_head]) {
-				this[_head] = item.next;
-			}
-
-			// Item is tail, unlink tail
-			if (item === this[_tail]) {
-				this[_tail] = last;
-			}
-
-			// If we have a last item, swap its next pointer
+	while (head) {
+		if (head.data === val) {
 			if (last) {
-				last.next = item.next;
+				last.next = head.next;
+			} else {
+				list = head.next;
 			}
-
-			// decrement length
-			this.length--;
-
-			// Stop looking
-			return false;
 		}
-
-		last = item;
-	});
-};
-
-LinkedList.prototype.forEach = function (fnc) {
-	var head;
-	while (head = (head ? head.next : this[_head])) { // eslint-disable-line no-cond-assign
-		// Early return by returning false
-		// from forEach callback
-		if (fnc.call(this, head) === false) {
-			return;
-		}
+		last = head;
+		head = head.next;
 	}
-};
 
-LinkedList.prototype.get = function (val) {
-	var match;
-	this.forEach(function (item) {
-		if (item.data !== val) {
-			return;
+	return list;
+}
+
+export function get (list, index) {
+	var i = 0;
+	while (list) {
+		if (i === index) {
+			return list;
 		}
-		match = item;
-		return false;
-	});
-	return match;
-};
+		list = list.next;
+		i++;
+	}
+}
 
-LinkedList.prototype.reverse = function () {
-	var extra;
+export function reverse (list) {
+	var tmp;
 	var prev = null;
-	while (this[_head]) {
-		extra = this[_head].next;
-		this[_head].next = prev;
-		prev = this[_head];
-		this[_head] = extra;
+	while (list) {
+		tmp = list.next;
+		list.next = prev;
+		prev = list;
+		list = tmp;
 	}
-	this[_head] = prev;
-};
+	return prev;
+}
 
-LinkedList.prototype.sort = function () {
+/*
+export function sort () {
 	// @TODO
 };
 
-LinkedList.merge = function (a, b) {
+export function merge (a, b) {
 	// @TODO
 };
+*/
--- test/index.js
+++ test/index.js
@@ -1,66 +1,113 @@
 /* global describe, it */
 var assert = require('assert');
-var LinkedList = require('../');
+var ll = require('../');
+var List = ll.List;
+var tail = ll.tail;
+var append = ll.append;
+var prepend = ll.prepend;
+var forEach = ll.forEach;
+var remove = ll.remove;
+var get = ll.get;
+var reverse = ll.reverse;
+var length = ll.length;
 
 describe('LinkedList', function () {
-	it('should create a linked list instance', function () {
-		var ll = new LinkedList();
-		assert.equal(ll.length, 0);
-		assert.equal(LinkedList.head(ll), null);
-		assert.equal(LinkedList.tail(ll), null);
+	it('should create a new linked list', function () {
+		// From nothing
+		assert.equal(List(), null);
+
+		// From the head balue
+		var list = List('foo');
+		assert.equal(list.data, 'foo');
+		assert.equal(length(list), 1);
+
+		// From multiple arguments
+		var list2 = List('foo', 'bar');
+		assert.equal(list2.data, 'foo');
+		assert.equal(list2.next.data, 'bar');
+		assert.equal(length(list2), 2);
+
+		// From an array
+		var list3 = List(['foo', 'bar']);
+		assert.equal(list3.data, 'foo');
+		assert.equal(list3.next.data, 'bar');
+		assert.equal(length(list3), 2);
 	});
 
 	it('should append an item to the linked list', function () {
-		var ll = new LinkedList();
-		ll.append('foo');
-		assert.equal(ll.length, 1);
-		assert.equal(LinkedList.head(ll), 'foo');
-		assert.equal(LinkedList.tail(ll), 'foo');
-		ll.append('bar');
-		assert.equal(ll.length, 2);
-		assert.equal(LinkedList.head(ll), 'foo');
-		assert.equal(LinkedList.tail(ll), 'bar');
-		ll.append('baz');
-		assert.equal(ll.length, 3);
-		assert.equal(LinkedList.head(ll), 'foo');
-		assert.equal(LinkedList.tail(ll), 'baz');
+		var ll = List('foo');
+
+		ll = append(ll, 'bar');
+		assert.equal(length(ll), 2);
+		assert.equal(ll.data, 'foo');
+		assert.equal(tail(ll).data, 'bar');
+
+		ll = append(ll, 'baz');
+		assert.equal(length(ll), 3);
+		assert.equal(ll.data, 'foo');
+		assert.equal(tail(ll).data, 'baz');
 	});
 
 	it('should prepend an item to the linked list', function () {
-		var ll = new LinkedList();
-		ll.prepend('foo');
-		assert.equal(ll.length, 1);
-		assert.equal(LinkedList.head(ll), 'foo');
-		assert.equal(LinkedList.tail(ll), 'foo');
-		ll.prepend('bar');
-		assert.equal(ll.length, 2);
-		assert.equal(LinkedList.head(ll), 'bar');
-		assert.equal(LinkedList.tail(ll), 'foo');
-		ll.prepend('baz');
-		assert.equal(ll.length, 3);
-		assert.equal(LinkedList.head(ll), 'baz');
-		assert.equal(LinkedList.tail(ll), 'foo');
+		var ll = List('foo');
+
+		ll = prepend(ll, 'bar');
+		assert.equal(length(ll), 2);
+		assert.equal(ll.data, 'bar');
+		assert.equal(tail(ll).data, 'foo');
+
+		ll = prepend(ll, 'baz');
+		assert.equal(length(ll), 3);
+		assert.equal(ll.data, 'baz');
+		assert.equal(tail(ll).data, 'foo');
+	});
+
+	it('should iterate each items in the list', function () {
+		var ll = List('foo', 'bar', 'baz');
+		forEach(ll, function (node, i) {
+			if (i === 0) {
+				assert.equal(node.data, 'foo');
+			}
+			if (i === 1) {
+				assert.equal(node.data, 'bar');
+			}
+			if (i === 2) {
+				assert.equal(node.data, 'baz');
+			}
+		});
 	});
 
 	it('should remove an item from the linked list', function () {
-		var ll = new LinkedList();
-		ll.append('foo');
-		ll.append('bar');
-		ll.append('baz');
+		var ll = List('foo', 'bar', 'baz');
 
-		ll.remove('foo');
-		assert.equal(ll.length, 2);
-		assert.equal(LinkedList.head(ll), 'bar');
-		assert.equal(LinkedList.tail(ll), 'baz');
+		ll = remove(ll, 'foo');
+		assert.equal(length(ll), 2);
+		assert.equal(ll.data, 'bar');
+		assert.equal(tail(ll).data, 'baz');
 
-		ll.remove('bar');
-		assert.equal(ll.length, 1);
-		assert.equal(LinkedList.head(ll), 'baz');
-		assert.equal(LinkedList.tail(ll), 'baz');
+		ll = remove(ll, 'bar');
+		assert.equal(length(ll), 1);
+		assert.equal(ll.data, 'baz');
+		assert.equal(tail(ll).data, 'baz');
 
-		ll.remove('baz');
-		assert.equal(ll.length, 0);
-		assert.equal(LinkedList.head(ll), null);
-		assert.equal(LinkedList.tail(ll), null);
+		ll = remove(ll, 'baz');
+		assert.equal(length(ll), 0);
+		assert.equal(ll, null);
+		assert.equal(tail(ll), null);
+	});
+
+	it('should get a node at a given index', function () {
+		var ll = List('foo', 'bar', 'baz');
+		assert.equal(get(ll, 0).data, 'foo');
+		assert.equal(get(ll, 1).data, 'bar');
+		assert.equal(get(ll, 2).data, 'baz');
+	});
+
+	it('should get reverse a list', function () {
+		var ll = List('foo', 'bar', 'baz');
+		ll = reverse(ll);
+		assert.equal(get(ll, 0).data, 'baz');
+		assert.equal(get(ll, 1).data, 'bar');
+		assert.equal(get(ll, 2).data, 'foo');
 	});
 });
